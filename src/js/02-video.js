2023f568f3b08d8ac5dd88caab46e7f74a6323c3
Andrey Yurchenko <spree92spree92@gmail.com>
2022-09-25 20:24:15 +0300

    complete hw2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onUpdate, 1000));
+
+function onUpdate(data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+  console.log(data);
+}
+
+const playerTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+if (playerTime === null) {
+  player.setCurrentTime(0);
+} else {
+  player.setCurrentTime(playerTime);
+}
